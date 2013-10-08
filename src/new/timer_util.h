85af6824c81fa701f6283fb123acd8690b27118c
Raghunath Rajachandrasekar <rajachan@cse.ohio-state.edu>
2013-10-08 19:20:34 -0400

    Building the timer_util sources.

--- src/new/timer_util.h
+++ src/new/timer_util.h
@@ -3,9 +3,6 @@
 
 #include <stdio.h>
 
-int take_timestamp (const char * label);
-int print_timestamps (FILE * fd);
-
 int begin_delta (const char * label);
 void end_delta (int delta_id);
 void print_deltas (FILE * fd);
