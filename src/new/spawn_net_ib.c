733ea18644177f42fd461054cc61b0eb41d34298
Adam Moody <moody20@llnl.gov>
2013-11-14 11:53:45 -0800

    move error checking to ib_internal

--- src/new/spawn_net_ib.c
+++ src/new/spawn_net_ib.c
@@ -13,14 +13,9 @@
 
 #include "spawn_internal.h"
 
-extern int my_pg_rank;
-extern int my_pg_size;
-
 spawn_net_endpoint* spawn_net_open_ib()
 {
     int nchild = 8;
-
-    my_pg_rank = PG_RANK;
 
     mv2_hca_open();
 
@@ -42,9 +37,6 @@ spawn_net_channel* spawn_net_connect_ib(const char* name)
 {
     comm_lock();
     spawn_net_channel* ch = mv2_ep_connect(name);
-    if (ch == SPAWN_NET_CHANNEL_NULL) {
-        SPAWN_ERR("Error creating IB communication channel");
-    }
     comm_unlock();
 
     return ch;
@@ -54,9 +46,6 @@ spawn_net_channel* spawn_net_accept_ib(const spawn_net_endpoint* ep)
 {
     comm_lock();
     spawn_net_channel* ch = mv2_ep_accept();
-    if (ch == SPAWN_NET_CHANNEL_NULL) {
-        SPAWN_ERR("Error accepting IB connection");
-    }
     comm_unlock();
 
     return ch;
@@ -73,9 +62,6 @@ int spawn_net_read_ib(const spawn_net_channel* ch, void* buf, size_t size)
 {
     comm_lock();
     int ret = mv2_ud_recv(ch, buf, size);
-    if (ret < 0) {
-        SPAWN_ERR("Couldn't recv IB message");
-    }
     comm_unlock();
 
     return ret;
@@ -85,9 +71,6 @@ int spawn_net_write_ib(const spawn_net_channel* ch, const void* buf, size_t size
 {
     comm_lock();
     int ret = mv2_ud_send(ch, buf, size);
-    if (ret < 0) {
-        SPAWN_ERR("Couldn't send IB message");
-    }
     comm_unlock();
 
     return ret;
