64b6be59c7718fecc31abed98ededda75cfe93f4
Adam Moody <moody20@llnl.gov>
2014-10-06 23:54:11 -0700

    create readlibs.c to hold fns to read elf data, create readlibs example

---
+++ src/new/examples/readlibs.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "spawn.h"
+
+/* return number of LIBS in map */
+static int lib_num(strmap* map)
+{
+    int num = 0;
+    const char* count = strmap_get(map, "LIBS");
+    if (count != NULL) {
+        num = atoi(count);
+    }
+    return num;
+}
+
+int main(int argc, char* argv[])
+{
+  int i;
+
+  if (argc != 2) {
+    printf("Usage: readlibs <exe>\n");
+    return 1;
+  }
+
+  const char* file = argv[1];
+
+  strmap* map = strmap_new();
+  lib_capture(map, file);
+
+  int libs = lib_num(map);
+  for (i = 0; i < libs; i++) {
+    const char* libname = strmap_getf(map, "LIB%d", i);
+    printf("%s\n", libname);
+  }
+
+  strmap_delete(&map);
+
+  return 0;
+}
---
+++ src/new/readlibs.h
@@ -0,0 +1,10 @@
+#ifndef READLIBS_H_
+#define READLIBS_H_
+
+#include "spawn.h"
+
+/* given a path to an executable, lookup its list of libraries
+ * and record the full path to each in the given map */
+void lib_capture(strmap* map, const char* file);
+
+#endif /* READLIBS_H_ */
